fdde4ec28fef2997d9bc5854aec0ed410511228a
ajitborade <ajitborade143@gmail.com>
2022-01-12 17:23:09 +0530

    hit the enter for spring boot swagger

---
+++ SpringBootGetbyAnyName/src/main/java/com/csi/model/Employee.java
@@ -0,0 +1,26 @@
+package com.csi.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int empId;
+    private String empFirstName;
+    private String empLastName;
+    private long empContactNo;
+    private String empEmailId;
+    private long empUid;
+    @JsonFormat (pattern = "dd-MM-yyyy")
+    private Date empDOB;
+
+
+}
